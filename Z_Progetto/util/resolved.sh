27089a62b77acb2859a6b4c61ac0ab2f7334d30f
Edoardo Mich <71270665+edoardomich@users.noreply.github.com>
2021-12-13 14:35:16 +0100

    Correct shell scripts

--- Z_Progetto/util/resolved.sh
+++ Z_Progetto/util/resolved.sh
@@ -3,9 +3,13 @@
 echo "Stopping systemd-resolved.service"
 systemctl stop systemd-resolved.service;
 
+echo "Disabling systemd-resolved.service"
+systemctl disable systemd-resolved.service;
+
 RFILE='/etc/resolv.conf';
 if [[ -L "$RFILE" ]]
 then
   echo "Removing symlink to /run/systemd/resolve/stub-resolv.conf"
-  $(mv $RFILE $RFILE.org; echo "NGN Project" > $RFILE);
+  $(mv $RFILE $RFILE.org);
+  $(echo "# NGN Project" > $RFILE);
 fi
--- Z_Progetto/util/resolved_restore.sh
+++ Z_Progetto/util/resolved_restore.sh
@@ -4,9 +4,13 @@ RFILE='/etc/resolv.conf';
 if [[ -e "$RFILE" ]]
 then
   $(rm $RFILE)
-  echo "Restoring symlink to /run/systemd/resolve/stub-resolv.conf"
-  $(mv $RFILE.org $RFILE)
 fi
+
+echo "Restoring symlink to /run/systemd/resolve/stub-resolv.conf"
+$(mv $RFILE.org $RFILE)
 
 echo "Starting systemd-resolved.service"
 systemctl start systemd-resolved.service;
+
+echo "Enabling systemd-resolved.service"
+systemctl enable systemd-resolved.service;
